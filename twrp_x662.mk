95882451ed4418bdd8ddc874737c8c8a7f341d75
Joe <109602792+Xixada@users.noreply.github.com>
2022-12-08 20:18:36 -0500

    x662: Initial Tree

---
+++ twrp_x662.mk
@@ -0,0 +1,16 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from fleur device Ho
+$(call inherit-product, device/xiaomi/fleur/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := x662
+PRODUCT_NAME := twrp_x662
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix Hot 11
+PRODUCT_MANUFACTURER := infinix
